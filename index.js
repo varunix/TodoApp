0de156f47ff52dab5a697dc0a2fa6c2f7e3a78c4
Varun Chaubey <varunchaubey4@gmail.com>
2022-07-27 17:42:19 +0530

    created form in html

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.static(__dirname+'/assets'));
 
 app.get('/', (req, res)=>{
     res.render('home', {
