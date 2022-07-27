98e3ed8ac7e7fe6d1be20dce315b5df066910c14
Varun Chaubey <varunchaubey4@gmail.com>
2022-07-28 02:27:31 +0530

    fixed drop down list

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,11 @@ var categoryTag = document.getElementById("category-div");
 var dropmenuTag = document.getElementById("dropdown-menu");
 var dropdownHeadingTag = document.getElementById("dropdown-heading");
 
-function dropdownVal(strList){
-    dropdownHeadingTag.innerHTML = strList;
-    dropmenuTag.style.display = "none";
-}
-
-categoryTag.addEventListener('click', function(){
+dropdownHeadingTag.addEventListener('click', function(){
     dropmenuTag.style.display = "block";
 });
+
+function listFunction(listVar){
+    dropdownHeadingTag.innerHTML = listVar;
+    dropmenuTag.style.display = "none";
+};
