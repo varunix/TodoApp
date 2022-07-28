0102e863f004dc88a7b404aaa8e958704bb0926d
Varun Chaubey <varunchaubey4@gmail.com>
2022-07-28 17:51:17 +0530

    added html for date-picker

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var categoryTag = document.getElementById("category-div");
 var dropmenuTag = document.getElementById("dropdown-menu");
 var dropdownHeadingTag = document.getElementById("dropdown-heading");
+var flag = false;
 
 dropdownHeadingTag.addEventListener('click', function(){
     dropmenuTag.style.display = "block";
@@ -10,3 +11,8 @@ function listFunction(listVar){
     dropdownHeadingTag.innerHTML = listVar;
     dropmenuTag.style.display = "none";
 };
+
+$(function(){
+    $("datepicker").datepicker();
+    dateFormat: "dd-mm-yyyy";
+});
